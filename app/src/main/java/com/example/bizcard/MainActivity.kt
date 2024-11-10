4d0c29c2f95b972db060c59253653cd805eb537a
JohnYCLam <johnyclam@gmail.com>
2024-11-10 11:45:32 +1100

    Added textboxes

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +21,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -63,25 +68,53 @@ fun CreateBizCard() {
             colors = CardDefaults.cardColors(containerColor = Color.White)
         ) {
 
-
-
-            Surface(
+            Column(
                 modifier = Modifier
-                    .size(150.dp)
-                    .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(1.dp, Color.LightGray),
-                tonalElevation = 4.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                    .height(300.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile_pic),
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier.size(135.dp),
-                    contentScale = ContentScale.Crop
-                )
+                CreateImageProfile()
+
+                HorizontalDivider(thickness = 1.dp,
+                     color = Color.LightGray)
+
+                Column(modifier = Modifier.padding(5.dp)) {
+                    Text(text = "John Lam",
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.colorScheme.primary)
+
+                    Text(text = "Machine Learning and AI Enthusiast",
+                        style = MaterialTheme.typography.bodyMedium)
+
+                    Text(text = "https://www.linkedin.com/in/john-lam-au/",
+                        style = MaterialTheme.typography.bodyMedium)
+                }
             }
+
+
         }
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(1.dp, Color.LightGray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_pic),
+            contentDescription = "Profile Picture",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
